9be9dafc2ab46890bc87f5737731fdeb203df587
Shamimzz <shamimislam5091@gmail.com>
2023-03-26 13:14:14 +0600

    adminApp

--- first/urls.py
+++ first/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls.conf import include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('product/', include('products.urls')),
     path('view/', include('view.urls')),
     path('payments/', include('payments.urls')),
--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from payments.models import pay_method
 
 # Register your models here.
+
+#it is used to display the organige model in admin panel
+class pay_methodAdmin(admin.ModelAdmin):
+    list_display = ['id','pay_id', 'pay_option', 'min_pay']
+    class Meta:
+        model = pay_method
+
+#it is used to register the model in admin panel
+admin.site.register(pay_method, pay_methodAdmin) 
+
