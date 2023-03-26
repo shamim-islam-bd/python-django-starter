f385c79b1b9b4e4183b71034b3886c71d0cd1590
Shamimzz <shamimislam5091@gmail.com>
2023-03-26 12:44:47 +0600

    database

--- payments/urls.py
+++ payments/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.payments, name='payments-1'),
     path('pay2/', views.payments2, name='payments-2'),
+    path('pay/', views.payment_method, name='pay'),
 ]
--- payments/views.py
+++ payments/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from payments.models import pay_method
 
 # Create your views here.
 def payments(request):
@@ -6,3 +7,7 @@ def payments(request):
 
 def payments2(request):
     return render(request, 'payments/payments-2.html', {})
+
+def payment_method(request):
+    pay_m = pay_method.objects.all()
+    return render(request, 'payments/pay.html', {'pay': pay_m})
